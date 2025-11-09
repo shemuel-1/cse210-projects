e5c7ab15f83589ff90fdd39f3d7f4b50265a8c1e
shemuel-1 <samuelpaintsil246@gmail.com>
2025-11-09 07:29:14 +0000

    Finished abstraction learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Sofware Engineer";
+        job1._company = "Microsoft";
+        job1._endYear = 2070;
+        job1._startYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Graphic Designer";
+        job2._company = "Meta";
+        job2._endYear = 2070;
+        job2._startYear = 2025;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Samuel";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
